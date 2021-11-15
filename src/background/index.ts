c9a28a792a545762a3935e239c6e5ced771513c4
林书豪 <shuhao_lin@fzzixun.com>
2021-11-15 09:31:25 +0800

    feat: 调整配置

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,9 @@
-import { ActionType } from "~/actionType/PopupToConTent"
+import { ActionType } from '~/actionType/PopupToConTent'
 
 // 默认点击打开
 enum From {
   CONTENT = 'content',
-  POPUP = 'popup'
+  POPUP = 'popup',
 }
 
 const windowInfo = {
@@ -18,20 +18,20 @@ chrome.browserAction.onClicked.addListener(function () {
     windowInfo.windowId = tab.windowId
     windowInfo.tabsId = tab.id
 
-    chrome.windows.create({
-      type: 'popup',
-      url: chrome.runtime.getURL('design.html'),
-    }, (popup) => {
-      windowInfo.popupId = popup.id
-    })
+    chrome.windows.create(
+      {
+        type: 'popup',
+        url: chrome.runtime.getURL('design.html'),
+      },
+      (popup) => {
+        windowInfo.popupId = popup.id
+      }
+    )
   })
 })
 
-
-const listener = (message: any,
-  sender: chrome.runtime.MessageSender,
-  sendResponse: (Response: any) => void) => {
-  console.log(sendResponse, sender);
+const listener = (message: any, sender: chrome.runtime.MessageSender, sendResponse: (Response: any) => void) => {
+  console.log(sendResponse, sender)
 
   const { from, type, data } = message
   switch (from) {
@@ -41,11 +41,15 @@ const listener = (message: any,
       {
         switch (type) {
           case ActionType.GET:
-            chrome.tabs.sendMessage(windowInfo.tabsId, {
-              type: ActionType.GET,
-            }, (shopList) => {
-              sendResponse(shopList)
-            })
+            chrome.tabs.sendMessage(
+              windowInfo.tabsId,
+              {
+                type: ActionType.GET,
+              },
+              (shopList) => {
+                sendResponse(shopList)
+              }
+            )
             break
           case ActionType.EDIT:
             chrome.tabs.sendMessage(windowInfo.tabsId, {
@@ -62,17 +66,12 @@ const listener = (message: any,
   return true
 }
 
-chrome.runtime.onMessageExternal.addListener((
-  message: any,
-  sender: chrome.runtime.MessageSender,
-  sendResponse: (Response: any) => void,
-) => listener(message, sender, sendResponse))
+chrome.runtime.onMessageExternal.addListener(
+  (message: any, sender: chrome.runtime.MessageSender, sendResponse: (Response: any) => void) =>
+    listener(message, sender, sendResponse)
+)
 
-
-chrome.runtime.onMessage.addListener((
-  message: any,
-  sender: chrome.runtime.MessageSender,
-  sendResponse: (Response: any) => void,
-) => listener(message, sender, sendResponse))
-
-
+chrome.runtime.onMessage.addListener(
+  (message: any, sender: chrome.runtime.MessageSender, sendResponse: (Response: any) => void) =>
+    listener(message, sender, sendResponse)
+)
